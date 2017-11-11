f0ecffa7809e78cb0e2b2dbd601d34d5bba776cf
Naganur <chandrashekar@taurusonline.net>
2017-11-10 17:41:18 -0800

    Adding Files

---
+++ EIMS1A design template/EIMS1A/Admindb/Dbservice.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+using EIMS1A.Models;
+using EIMS1A.Controllers;
+using EIMS1A.Admindb;
+
+
+namespace EIMS1A.Admindb
+{
+    public class Dbservice
+    {
+        public string conn = ConfigurationManager.AppSettings["MSSQL"].ToString();
+
+        public object GetEmployeeInfoByEmpid(EmpInfo b)
+        {
+            //SqlConnection con = new SqlConnection(conn);
+
+            //SqlCommand cmd1 = new SqlCommand("sp_initial_new_gettingVehicleregNo", con);
+            //cmd1.CommandType = CommandType.StoredProcedure;
+            //cmd1.Parameters.Add(new SqlParameter("@empid",emp_id));
+            //SqlDataAdapter sda = new SqlDataAdapter(cmd1);
+            //DataTable dt = new DataTable();
+            //con.Open();
+            //sda.Fill(dt);
+            //return b;
+            SqlConnection con1 = new SqlConnection(conn);
+            SqlCommand cmd1 = new SqlCommand();
+            cmd1.Connection = con1;
+            cmd1.CommandText = @"select  h.pay_commission, h.grade_pay_p6,h.date_of_next_increment,
+ b.emp_id,a.emp_name,c.designation,c.designation_id,d.dept_id,d.division_id,d.dept_name,a.category,
+ a.date_of_birth,b.date_of_superannuation,a.date_of_joining,f.division,b.employee_status,
+ b.status_to_date,g.basic,g.increment1,g.stagnation1,g.increment2,g.stagnation2,h.currentPay,
+ h.personal_pay_allowance,h.deputation_pay_allowance from employee_master a,employee_transaction b,
+ --VMaxEmpInfo e,
+ designation_master c,dept_master d ,division_master f ,payscale_master g,payscale_transaction h
+  where g.payscale_id=h.payscale_id and h.emp_id=a.emp_id and  f.div_id=d.division_id
+   and 
+   --b.transaction_id=e.transaction_id and 
+    a.emp_id=b.emp_id and  b.emp_id=b.emp_id and
+ b.emp_id=a.emp_id and b.designation_id=c.designation_id and d.dept_id=b.dept_id and
+ b.transaction_id in(select MAX(transaction_id) from Employee_Transaction group by emp_id) and
+    h.transaction_id in (select max(transaction_id) as transaction_id from payscale_transaction t 
+    where t.emp_id=h.emp_id ) and  b.emp_id='" + b.emp_id + "'";
+            con1.Open();
+            cmd1.Connection = con1;
+            SqlDataReader dr = cmd1.ExecuteReader();
+            while (dr.Read())
+            {
+               // b.emp_id = (int)dr["emp_id"];
+                b.emp_id = Convert.ToInt32(dr["emp_id"]);
+
+                b.emp_name = dr["emp_name"].ToString();
+                b.division = dr["division"].ToString();
+               // b.status_to_date = dr["status"].ToString();
+                b.designation =dr["designation"].ToString();
+                b.dept_name = dr["dept_name"].ToString();
+                b.category = dr["category"].ToString();
+              //  b.date_of_birth = b.date_of_birth.Date.ToString("MM/dd/yyyy");
+              //  b.currentPay = dr["payscale"].ToString();
+             //   b.date_of_next_increment = b.date_of_next_increment.Date.ToString("MM/dd/yyyy");
+
+               // b.currentPay = (int)dr["Registarationcharge"];
+                //b.ConsultationCharge = (int)dr["ConsultationCharge"];
+                //b.DressingCharge = (int)dr["DressingCharge"];
+                //b.PlasteringCharge = (int)dr["DressingCharge"];
+                //b.DressingCharge = (int)dr["PlasteringCharge"];
+                //b.InjectionCharge = (int)dr["InjectionCharge"];
+                //b.XRayCharge = (int)dr["XRayCharge"];
+                //b.OtherCharge = (int)dr["OtherCharge"];
+                //b.TotalCharge = (int)dr["TotalCharge"];
+                //b.PaymentMode = dr["Paymentmode"].ToString();
+                //b.TransactionID = dr["Transactionid"].ToString();
+
+                //d.DOB = d.DOB.Date.ToString("MM/dd/yyyy");
+
+            }
+            con1.Close();
+
+            return b;
+        }
+
+    }
+}
---
+++ EIMS1A design template/EIMS1A/js/script.js
@@ -0,0 +1,537 @@
+(function($) {
+	
+	"use strict";
+	
+	
+	//Hide Loading Box (Preloader)
+	function handlePreloader() {
+		if($('.preloader').length){
+			$('.preloader').delay(200).fadeOut(500);
+		}
+	}
+	
+	
+	//Update Header Style and Scroll to Top
+	function headerStyle() {
+		if($('.main-header').length){
+			var windowpos = $(window).scrollTop();
+			var siteHeader = $('.main-header');
+			var scrollLink = $('.scroll-to-top');
+			if (windowpos >= 200) {
+				siteHeader.addClass('fixed-header');
+				scrollLink.fadeIn(300);
+			} else {
+				siteHeader.removeClass('fixed-header');
+				scrollLink.fadeOut(300);
+			}
+		}
+	}
+	
+	headerStyle();
+	
+	
+	//Submenu Dropdown Toggle
+	if($('.main-header li.dropdown ul').length){
+		$('.main-header li.dropdown').append('<div class="dropdown-btn"></div>');
+		
+		//Dropdown Button
+		$('.main-header li.dropdown .dropdown-btn').on('click', function() {
+			$(this).prev('ul').slideToggle(500);
+		});
+		
+		//Disable dropdown parent link
+		$('.main-header .navigation li.dropdown > a,.hidden-bar .side-menu li.dropdown > a').on('click', function(e) {
+			e.preventDefault();
+		});
+	}
+	
+	
+	//Hidden Bar Menu Config
+	function hiddenBarMenuConfig() {
+		var menuWrap = $('.hidden-bar .side-menu');
+		// appending expander button
+		menuWrap.find('.dropdown').children('a').append(function () {
+			return '<button type="button" class="btn expander"><i class="icon fa fa-angle-down"></i></button>';
+		});
+		// hidding submenu 
+		menuWrap.find('.dropdown').children('ul').hide();
+		// toggling child ul
+		menuWrap.find('.btn.expander').each(function () {
+			$(this).on('click', function () {
+				$(this).parent() // return parent of .btn.expander (a) 
+					.parent() // return parent of a (li)
+						.children('ul').slideToggle();
+	
+				// adding class to expander container
+				$(this).parent().toggleClass('current');
+				// toggling arrow of expander
+				$(this).find('i').toggleClass('fa-angle-up fa-angle-down');
+	
+				return false;
+	
+			});
+		});
+	}
+	
+	hiddenBarMenuConfig();
+	
+	//Hidden Sidebar
+	if ($('.hidden-bar').length) {
+		var hiddenBar = $('.hidden-bar');
+		var hiddenBarOpener = $('.hidden-bar-opener');
+		var hiddenBarCloser = $('.hidden-bar-closer');
+		$('.hidden-bar-wrapper').mCustomScrollbar();
+		
+		//Show Sidebar
+		hiddenBarOpener.on('click', function () {
+			hiddenBar.addClass('visible-sidebar');
+		});
+		
+		//Hide Sidebar
+		hiddenBarCloser.on('click', function () {
+			hiddenBar.removeClass('visible-sidebar');
+		});
+		
+	}
+	
+	//Panels Toggle (Shopping Cart Page)
+	if ($('.toggle-panel').length) {
+		var targetPanel = $('.toggle-content');
+		
+		//Show Panel
+		$('.toggle-panel').on('click', function () {
+			$(this).toggleClass('active-panel');
+			$(this).next(targetPanel).fadeToggle(300);
+		});
+		
+	}
+	
+	
+	//Revolution Slider
+	if($('.main-slider .tp-banner').length){
+
+		jQuery('.main-slider .tp-banner').show().revolution({
+			//dottedOverlay:"yes",
+			delay:10000,
+			startwidth:1200,
+			startheight:680,
+			hideThumbs:600,
+			
+			thumbWidth:80,
+			thumbHeight:50,
+			thumbAmount:5,
+			
+			navigationType:"bullet",
+			navigationArrows:"0",
+			navigationStyle:"preview3",
+			
+			touchenabled:"on",
+			onHoverStop:"off",
+			
+			swipe_velocity: 0.7,
+			swipe_min_touches: 1,
+			swipe_max_touches: 1,
+			drag_block_vertical: false,
+			
+			parallax:"mouse",
+			parallaxBgFreeze:"on",
+			parallaxLevels:[7,4,3,2,5,4,3,2,1,0],
+			
+			keyboardNavigation:"off",
+			
+			navigationHAlign:"center",
+			navigationVAlign:"bottom",
+			navigationHOffset:0,
+			navigationVOffset:30,
+			
+			soloArrowLeftHalign:"left",
+			soloArrowLeftValign:"center",
+			soloArrowLeftHOffset:20,
+			soloArrowLeftVOffset:0,
+			
+			soloArrowRightHalign:"right",
+			soloArrowRightValign:"center",
+			soloArrowRightHOffset:20,
+			soloArrowRightVOffset:0,
+			
+			shadow:0,
+			fullWidth:"on",
+			fullScreen:"off",
+			
+			spinner:"spinner1",
+			
+			stopLoop:"off",
+			stopAfterLoops:-1,
+			stopAtSlide:-1,
+			
+			shuffle:"off",
+			
+			autoHeight:"off",
+			forceFullWidth:"off",
+			
+			hideThumbsOnMobile:"on",
+			hideNavDelayOnMobile:1500,
+			hideBulletsOnMobile:"on",
+			hideArrowsOnMobile:"on",
+			hideThumbsUnderResolution:0,
+			
+			hideSliderAtLimit:0,
+			hideCaptionAtLimit:0,
+			hideAllCaptionAtLilmit:0,
+			startWithSlide:0,
+			videoJsPath:"",
+			fullScreenOffsetContainer: ""
+	  });
+	}
+	
+	
+	//Tabs Box
+	if($('.tabs-box').length){
+		$('.tabs-box .tab-buttons .tab-btn').on('click', function(e) {
+			e.preventDefault();
+			var target = $($(this).attr('data-tab'));
+			
+			if ($(target).is(':visible')){
+				return false;
+			}else{
+				target.parents('.tabs-box').find('.tab-buttons').find('.tab-btn').removeClass('active-btn');
+				$(this).addClass('active-btn');
+				target.parents('.tabs-box').find('.tabs-content').find('.tab').fadeOut(0);
+				target.parents('.tabs-box').find('.tabs-content').find('.tab').removeClass('active-tab');
+				$(target).fadeIn(300);
+				$(target).addClass('active-tab');
+			}
+		});
+	}
+	
+	
+	// Donation Progress Bar
+	if ($('.animated-bar').length) {
+		
+		$('.animated-bar').appear(function(){
+			var el = $(this);
+			var percent = el.data('percent');
+			$(el).css('width',percent).addClass('counted');
+		},{accY: -50});
+
+	}
+	
+	
+	// Fact Counter
+	function factCounter() {
+		if($('.fact-counter').length){
+			$('.fact-counter .counter-column').appear(function(){
+		
+				var $t = $(this),
+					n = $t.find(".count-text").attr("data-stop"),
+					r = parseInt($t.find(".count-text").attr("data-speed"), 10);
+					
+				if (!$t.hasClass("counted")) {
+					$t.addClass("counted");
+					$({
+						countNum: $t.find(".count-text").text()
+					}).animate({
+						countNum: n
+					}, {
+						duration: r,
+						easing: "linear",
+						step: function() {
+							$t.find(".count-text").text(Math.floor(this.countNum));
+						},
+						complete: function() {
+							$t.find(".count-text").text(this.countNum);
+						}
+					});
+				}
+				
+			},{accY: -50});
+		}
+	}
+	
+	//Accordion Box
+	if($('.accordion-box').length){
+		$(".accordion-box").on('click', '.acc-btn', function() {
+			
+			var outerBox = $(this).parents('.accordion-box');
+			var target = $(this).parents('.accordion');
+			
+			if($(this).hasClass('active')!==true){
+			$('.accordion .acc-btn').removeClass('active');
+			
+			}
+			
+			if ($(this).next('.acc-content').is(':visible')){
+				return false;
+			}else{
+				$(this).addClass('active');
+				$(outerBox).children('.accordion').removeClass('active-block');
+				$(outerBox).children('.accordion').children('.acc-content').slideUp(300);
+				target.addClass('active-block');
+				$(this).next('.acc-content').slideDown(300);	
+			}
+		});	
+	}
+	
+	
+	//Event Countdown Timer
+	if($('.time-countdown').length){  
+		$('.time-countdown').each(function() {
+		var $this = $(this), finalDate = $(this).data('countdown');
+		$this.countdown(finalDate, function(event) {
+			var $this = $(this).html(event.strftime('' + '<div class="counter-column"><span class="count">%D</span>Days</div> ' + '<div class="counter-column"><span class="count">%H</span>Hours</div>  ' + '<div class="counter-column"><span class="count">%M</span>Minutes</div>  ' + '<div class="counter-column"><span class="count">%S</span>Seconds</div>'));
+		});
+	 });
+	}
+	
+	
+	//Sponsors Carousel
+	if ($('.sponsors-carousel').length) {
+		$('.sponsors-carousel').owlCarousel({
+			loop:true,
+			margin:40,
+			nav:true,
+			smartSpeed: 500,
+			autoplay: 5000,
+			navText: [ '<span class="fa fa-angle-left"></span>', '<span class="fa fa-angle-right"></span>' ],
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:3
+				},
+				800:{
+					items:4
+				},
+				1024:{
+					items:5
+				},
+				1200:{
+					items:6
+				}
+			}
+		});    		
+	}
+	
+	//Sponsors Carousel
+	if ($('.sponsors-carousel-two').length) {
+		$('.sponsors-carousel-two').owlCarousel({
+			loop:true,
+			margin:30,
+			nav:true,
+			smartSpeed: 500,
+			autoplay: 5000,
+			navText: [ '<span class="fa fa-angle-left"></span>', '<span class="fa fa-angle-right"></span>' ],
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:2
+				},
+				800:{
+					items:3
+				},
+				1024:{
+					items:4
+				},
+				1200:{
+					items:5
+				}
+			}
+		});    		
+	}
+	
+	
+	//Single Item Slider
+	if ($('.single-item-carousel').length) {
+		$('.single-item-carousel').owlCarousel({
+			loop:true,
+			margin:0,
+			nav:true,
+			smartSpeed: 700,
+			autoplay: 4000,
+			navText: [ '<span class="fa fa-angle-left"></span>', '<span class="fa fa-angle-right"></span>' ],
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:1
+				},
+				1200:{
+					items:1
+				}
+			}
+		});    		
+	}
+	
+	
+	//Sortable Masonary with Filters
+	function enableMasonry() {
+		if($('.sortable-masonry').length){
+	
+			var winDow = $(window);
+			// Needed variables
+			var $container=$('.sortable-masonry .items-container');
+			var $filter=$('.filter-btns');
+	
+			$container.isotope({
+				filter:'*',
+				 masonry: {
+					columnWidth : 1 
+				 },
+				animationOptions:{
+					duration:500,
+					easing:'linear'
+				}
+			});
+			
+	
+			// Isotope Filter 
+			$filter.find('li').on('click', function(){
+				var selector = $(this).attr('data-filter');
+	
+				try {
+					$container.isotope({ 
+						filter	: selector,
+						animationOptions: {
+							duration: 500,
+							easing	: 'linear',
+							queue	: false
+						}
+					});
+				} catch(err) {
+	
+				}
+				return false;
+			});
+	
+	
+			winDow.bind('resize', function(){
+				var selector = $filter.find('li.active').attr('data-filter');
+
+				$container.isotope({ 
+					filter	: selector,
+					animationOptions: {
+						duration: 500,
+						easing	: 'linear',
+						queue	: false
+					}
+				});
+			});
+	
+	
+			var filterItemA	= $('.filter-btns li');
+	
+			filterItemA.on('click', function(){
+				var $this = $(this);
+				if ( !$this.hasClass('active')) {
+					filterItemA.removeClass('active');
+					$this.addClass('active');
+				}
+			});
+		}
+	}
+	
+	enableMasonry();
+	
+	
+	//LightBox / Fancybox
+	if($('.lightbox-image').length) {
+		$('.lightbox-image').fancybox({
+			openEffect  : 'fade',
+			closeEffect : 'fade',
+			helpers : {
+				media : {}
+			}
+		});
+	}
+	
+	//Jquery Spinner / Quantity Spinner
+	if($('.quantity-spinner').length){
+		 $('.quantity-spinner .plus').on('click', function() {
+			var val = $(this).prev('.prod_qty').val();
+			$(this).prev('.prod_qty').val((val*1)+1);
+		});
+		$('.quantity-spinner .minus').on('click', function(){
+			var val = $(this).next('.prod_qty').val();
+			if (val != 1 ){
+			$(this).next('.prod_qty').val((val*1)-1);
+			}
+		});
+	}
+	
+	
+	//Contact Form Validation
+	if($('#contact-form').length){
+		$('#contact-form').validate({
+			rules: {
+				username: {
+					required: true
+				},
+				email: {
+					required: true,
+					email: true
+				},
+				subject: {
+					required: true
+				},
+				message: {
+					required: true
+				}
+			}
+		});
+	}
+	
+	
+	// Scroll to a Specific Div
+	if($('.scroll-to-target').length){
+		$(".scroll-to-target").on('click', function() {
+			var target = $(this).attr('data-target');
+		   // animate
+		   $('html, body').animate({
+			   scrollTop: $(target).offset().top
+			 }, 1500);
+	
+		});
+	}
+	
+	
+	// Elements Animation
+	if($('.wow').length){
+		var wow = new WOW(
+		  {
+			boxClass:     'wow',      // animated element css class (default is wow)
+			animateClass: 'animated', // animation css class (default is animated)
+			offset:       0,          // distance to the element when triggering the animation (default is 0)
+			mobile:       true,       // trigger animations on mobile devices (default is true)
+			live:         true       // act on asynchronously loaded content (default is true)
+		  }
+		);
+		wow.init();
+	}
+
+
+/* ==========================================================================
+   When document is Scrollig, do
+   ========================================================================== */
+	
+	$(window).on('scroll', function() {
+		headerStyle();
+		factCounter();
+	});
+	
+/* ==========================================================================
+   When document is loading, do
+   ========================================================================== */
+	
+	$(window).on('load', function() {
+		handlePreloader();
+		enableMasonry();
+	});
+	
+
+	
+
+})(window.jQuery);
